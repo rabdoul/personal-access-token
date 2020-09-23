247a9efecd0f65e7b3e9342ec6cdbf74df02205e
Barbe Julien <j.barbe@lectra.com>
2020-09-23 11:01:21 +0200

    Clean useRule

--- client/src/rules/useRule.ts
+++ client/src/rules/useRule.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 import { fetchData } from 'raspberry-fetch';
 
-import { useUIStateContext, UIState } from '../UIState';
+import { UIState, useUIState } from '../UIState';
 import { useAccessToken } from '../base/Authentication';
 
 type RuleId = keyof Omit<UIState, 'editMode'>;
@@ -10,7 +10,7 @@ type RuleId = keyof Omit<UIState, 'editMode'>;
 export default function useRule<R_ID extends RuleId, R>(ruleId: R_ID, init: (rule: R) => void): UIState[R_ID] {
   const accessToken = useAccessToken();
 
-  const [{ editMode, [ruleId]: editedRule }, dispatch] = useUIStateContext();
+  const { editMode, [ruleId]: editedRule } = useUIState();
 
   const { data, isSuccess } = useQuery<R>(ruleId, () => fetchData(accessToken, `activities/${ruleId}`));
 
@@ -18,7 +18,7 @@ export default function useRule<R_ID extends RuleId, R>(ruleId: R_ID, init: (rul
     if (editMode && isSuccess && !editedRule) {
       init(data!);
     }
-  }, [editMode, init, isSuccess, dispatch, data, editedRule]);
+  }, [editMode, init, isSuccess, data, editedRule]);
 
   return editedRule ?? data;
 }
