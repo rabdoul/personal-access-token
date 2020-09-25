3b086d4d1348e52061e00ba507cf83cd5e017977
b.prioux <b.prioux@lectra.com>
2020-09-25 11:12:25 +0200

    disable save button if error

--- client/src/ribbon/__test__/useRibbonConfig.spec.tsx
+++ client/src/ribbon/__test__/useRibbonConfig.spec.tsx
@@ -35,9 +35,9 @@ describe('useRibbonConfig', () => {
     expect(config.groups[0].commands[0].enable).toBeTruthy();
   });
 
-  it('ribbon should render edit ribbon config', () => {
+  it('ribbon should render edit ribbon config with save enabled', () => {
     const { result } = renderHook(() => useRibbonConfig(), {
-      wrapper: ({ children }) => <MockedProviders uiStateContext={[{ editMode: true }, () => {}]}>{children}</MockedProviders>
+      wrapper: ({ children }) => <MockedProviders uiStateContext={[{ editMode: true, invalidRules: { 'setup-sequencing': new Set() } }, () => {}]}>{children}</MockedProviders>
     });
 
     const config = result.current;
@@ -50,4 +50,28 @@ describe('useRibbonConfig', () => {
     expect(config.groups[0].commands[1].description).toEqual('Cancel');
     expect(config.groups[0].commands[1].enable).toBeTruthy();
   });
+
+  it('ribbon should render edit ribbon config with save disabled', () => {
+    const { result } = renderHook(() => useRibbonConfig(), {
+      wrapper: ({ children }) => (
+        <MockedProviders uiStateContext={[{ editMode: true, invalidRules: { 'setup-sequencing': setOf('numberOfProductOrders') } }, () => {}]}>{children}</MockedProviders>
+      )
+    });
+
+    const config = result.current;
+
+    expect(config.groups.length).toEqual(1);
+    expect(config.groups[0].description).toEqual('Save');
+    expect(config.groups[0].commands.length).toEqual(2);
+    expect(config.groups[0].commands[0].description).toEqual('Save');
+    expect(config.groups[0].commands[0].enable).toBeFalsy();
+    expect(config.groups[0].commands[1].description).toEqual('Cancel');
+    expect(config.groups[0].commands[1].enable).toBeTruthy();
+  });
 });
+
+function setOf(value: string) {
+  const set = new Set();
+  set.add(value);
+  return set;
+}
