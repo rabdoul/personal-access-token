d7cd13614798e1aff58d17bc9994b6355eb67f4a
b.prioux <b.prioux@lectra.com>
2020-10-13 18:01:42 +0200

    add unit test for support mode

--- client/src/ribbon/__test__/useRibbonConfig.spec.tsx
+++ client/src/ribbon/__test__/useRibbonConfig.spec.tsx
@@ -1,10 +1,11 @@
 import 'jest';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
 import { IntlProvider } from 'react-intl';
 
 import useRibbonConfig from '../useRibbonConfig';
 import { UIStateContext } from '../../UIState';
+import { AuthenticationContext, AuthenticatedUserContext } from '../../base/Authentication';
 
 const i18nMessages = {
   'group.edit': 'Edit',
@@ -14,16 +15,24 @@ const i18nMessages = {
   'command.edit': 'Edit'
 };
 
-const MockedProviders = ({ uiStateContext, children }: { uiStateContext: any; children: any }) => (
-  <IntlProvider locale="en" messages={i18nMessages}>
-    <UIStateContext.Provider value={uiStateContext}>{children}</UIStateContext.Provider>
-  </IntlProvider>
+const buildAuthContext = (accountType: string) => new AuthenticatedUserContext('', '', { 'https://metadata.lectra.com/app_metadata': { account_type: accountType } });
+
+const MockedProviders = ({ uiStateContext, children, accountType }: { uiStateContext: any; children: any; accountType: string }) => (
+  <AuthenticationContext.Provider value={buildAuthContext(accountType)}>
+    <IntlProvider locale="en" messages={i18nMessages}>
+      <UIStateContext.Provider value={uiStateContext}>{children}</UIStateContext.Provider>
+    </IntlProvider>
+  </AuthenticationContext.Provider>
 );
 
 describe('useRibbonConfig', () => {
   it('ribbon should render default ribbon config', () => {
     const { result } = renderHook(() => useRibbonConfig(), {
-      wrapper: ({ children }) => <MockedProviders uiStateContext={[{ editMode: false }, () => {}]}>{children}</MockedProviders>
+      wrapper: ({ children }) => (
+        <MockedProviders accountType="USER" uiStateContext={[{ editMode: false }, () => {}]}>
+          {children}
+        </MockedProviders>
+      )
     });
 
     const config = result.current;
@@ -37,7 +46,11 @@ describe('useRibbonConfig', () => {
 
   it('ribbon should render edit ribbon config with save enabled', () => {
     const { result } = renderHook(() => useRibbonConfig(), {
-      wrapper: ({ children }) => <MockedProviders uiStateContext={[{ editMode: true, invalidRules: new Set() }, () => {}]}>{children}</MockedProviders>
+      wrapper: ({ children }) => (
+        <MockedProviders accountType="USER" uiStateContext={[{ editMode: true, invalidRules: new Set() }, () => {}]}>
+          {children}
+        </MockedProviders>
+      )
     });
 
     const config = result.current;
@@ -51,10 +64,32 @@ describe('useRibbonConfig', () => {
     expect(config.groups[0].commands[1].enable).toBeTruthy();
   });
 
-  it('ribbon should render edit ribbon config with save disabled', () => {
+  it('ribbon should render edit ribbon config with save disabled when error', () => {
     const { result } = renderHook(() => useRibbonConfig(), {
       wrapper: ({ children }) => (
-        <MockedProviders uiStateContext={[{ editMode: true, invalidRules: { 'setup-sequencing': new Set(['numberOfProductOrders']) } }, () => {}]}>{children}</MockedProviders>
+        <MockedProviders accountType="USER" uiStateContext={[{ editMode: true, invalidRules: { 'setup-sequencing': new Set(['numberOfProductOrders']) } }, () => {}]}>
+          {children}
+        </MockedProviders>
+      )
+    });
+
+    const config = result.current;
+
+    expect(config.groups.length).toEqual(1);
+    expect(config.groups[0].description).toEqual('Save');
+    expect(config.groups[0].commands.length).toEqual(2);
+    expect(config.groups[0].commands[0].description).toEqual('Save');
+    expect(config.groups[0].commands[0].enable).toBeFalsy();
+    expect(config.groups[0].commands[1].description).toEqual('Cancel');
+    expect(config.groups[0].commands[1].enable).toBeTruthy();
+  });
+
+  it('ribbon should render edit ribbon config with save disabled when mode support', () => {
+    const { result } = renderHook(() => useRibbonConfig(), {
+      wrapper: ({ children }) => (
+        <MockedProviders accountType="SUPPORT" uiStateContext={[{ editMode: true, invalidRules: new Set() }, () => {}]}>
+          {children}
+        </MockedProviders>
       )
     });
 
